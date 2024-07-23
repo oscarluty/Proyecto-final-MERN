fe9b3c3cc45b49b5efe63fc1f515c7ae0275378c
oscarluty <oscar.luty@gmail.com>
2024-07-22 20:51:53 -0400

    Fix deployment issues

--- server/config.js
+++ server/config.js
@@ -1,4 +1,4 @@
 export const PORT = process.env.PORT || 4000;
 export const TOKEN_SECRET = process.env.TOKEN_SECRET || 'some_secret_key_muy_seguro_y_largo';
-export const FRONTEND_URL = process.env.FRONTEND_URL || "http://localhost:4000";
+export const FRONTEND_URL = process.env.FRONTEND_URL || "http://localhost:3004";
 export const MONGODB_URI = process.env.MONGODB_URI || 'mongodb+srv://oscarluty:J67lA0E9zlBFzCqb@arrierocluster.elbacgd.mongodb.net/arrierodb';
