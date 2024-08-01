d98372133168f28d505aaa78a96fedb748f43f6f
oscarluty <oscar.luty@gmail.com>
2024-07-31 20:25:56 -0400

    Actualizando variables de entorno

--- src/api/fetchProduct.js
+++ src/api/fetchProduct.js
@@ -1,12 +1,17 @@
+import { API_URL } from "../config";
+
 export const fetchProducts = async () => {
   try {
-    const response = await fetch('http://localhost:4000/api/products');
+    const response = await fetch(API_URL+'/products');
     if (!response.ok) {
       throw new Error(`HTTP error! status: ${response.status}`);
     }
     return await response.json();
   } catch (error) {
     console.error('Error fetching products:', error);
+    if (error instanceof TypeError && error.message.includes('CORS')) {
+      console.log('CORS error detectado. Por favor checkear configuracion CORS en el servidor');
+    }
     throw error;
   }
 };
