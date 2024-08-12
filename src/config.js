e055168f2400f935e4337c7948952b3da51a2d98
oscarluty <oscar.luty@gmail.com>
2024-08-11 21:17:39 -0400

    cambiando api url

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 const isProduction = import.meta.env.PROD;
 
 export const API_URL = isProduction
-  ? import.meta.env.VITE_API_URL || "https://serene-meadow-83357-d161acbcd0d7.herokuapp.com/api"
+  ? import.meta.env.VITE_API_URL || "www.arrierombarete.com"
   : "http://localhost:4000/api";
 
 console.log(`Frontend running in ${isProduction ? 'production' : 'development'} mode`);
